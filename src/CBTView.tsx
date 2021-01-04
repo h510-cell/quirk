9c11ea7385c76ba42a94350184336b4140b5dcf1
h510-cell <58731327+h510-cell@users.noreply.github.com>
2021-01-04 16:35:37 +0530

    Update CBTView.tsx

--- src/CBTView.tsx
+++ src/CBTView.tsx
@@ -12,7 +12,7 @@ const cognitiveDistortionsToText = cognitiveDistortions => {
       <Paragraph
         key={slug}
         style={{
-          marginBottom: 8,
+          marginBottom: 10,
         }}
       >
         {emojiForSlug(slug)} {label}
@@ -37,7 +37,7 @@ export default ({ thought }: ThoughtComponent) => (
       {thought.automaticThought ? (
         <BubbleThought
           style={{
-            marginTop: 0,
+            marginTop: 1,
           }}
         >
           {thought.automaticThought}
@@ -62,7 +62,7 @@ export default ({ thought }: ThoughtComponent) => (
       {thought.alternativeThought ? (
         <BubbleThought
           style={{
-            marginTop: 0,
+            marginTop: 1,
           }}
           color="pink"
         >
